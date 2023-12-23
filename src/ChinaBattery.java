05e2f2155fb5cef8f73cf57bcba2b291042b5c1c
Nat167 <chery123@mail.ru>
2023-12-23 22:27:14 +0400

    Add Main01.java, Flashlight.java, ChinaBattery.java

---
+++ src/ChinaBattery.java
@@ -0,0 +1,14 @@
+public class ChinaBattery {
+
+    private int charge = 5;
+
+    public boolean charge() {
+        if (charge > 0) {
+            charge--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
